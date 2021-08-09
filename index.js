436af5599bcd97e3f77e7ea41be7b73c3d75087a
Wilfred <wilfredbayudan@gmail.com>
2021-08-08 17:34:32 -1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "WILFRED is the champion";
+
+document.body.append(newHeader);
